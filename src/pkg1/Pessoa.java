6b1556bc2e4ed4881e7ae19508e5f881a9bfe7bd
Antonielson Silva <antonielsom19@gmail.com>
2022-03-14 00:14:46 -0300

    Primeira Aula

---
+++ src/pkg1/Pessoa.java
@@ -0,0 +1,49 @@
+package pkg1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Pessoa
+ */
+@WebServlet("/Pessoa")
+public class Pessoa extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Pessoa() {
+        // TODO Auto-generated constructor stub
+    }
+
+    
+    	 
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		
+		out.println("<p>Nome: " + request.getParameter("nome") + "</p>");
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
